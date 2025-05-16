2c807b5b91fecd80bb4877ed26170b29eafd78a9
sophie <shafiyahnuril@gmail.com>
2025-05-16 12:05:10 +0700

    runtime.js solved

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 // src/index.js - File entri utama untuk aplikasi React
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App.js';
-import './index.css';
-import { Web3Provider } from './contexts/Web3Context.js';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import App from './App.js';
+// import './index.css';
+// import { Web3Provider } from './contexts/Web3Context.js';
+// import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.js';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Web3Provider>
+//       <Router>
+//         <App />
+//       </Router>
+//     </Web3Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Web3Provider>
-      <Router>
-        <App />
-      </Router>
-    </Web3Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
